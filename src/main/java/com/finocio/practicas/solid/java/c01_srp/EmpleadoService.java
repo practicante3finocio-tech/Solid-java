34dcbda46c233606618b875812c2863b23f16393
Jose <practicante3finocio@gmail.com>
2026-01-07 10:49:54 +0100

    feat: add srp package with Empleado, services, and report logic, EmpleadoService, ReporteServicio, SalarioServicio

---
+++ src/main/java/com/finocio/practicas/solid/java/c01_srp/EmpleadoService.java
@@ -0,0 +1,12 @@
+package com.finocio.practicas.solid.java.c01_srp;
+
+public class EmpleadoService {
+
+    public Empleado crearEmpleado(Empleado empleado) {
+        return null;
+    }
+
+    public Empleado obtenerEmpleado(long id) {
+        return null;
+    }
+}
